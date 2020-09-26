43ac80f6a4caafcec4879525fb7e20019606ba2a
Rongmario <ethanmahmood@gmail.com>
2020-09-26 03:24:36 +0100

    Stop empty/null stacks from being queried

--- src/main/java/zone/rong/thaumicspeedup/mixins/ThaumcraftApiMixin.java
+++ src/main/java/zone/rong/thaumicspeedup/mixins/ThaumcraftApiMixin.java
@@ -3,13 +3,30 @@ package zone.rong.thaumicspeedup.mixins;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.aspects.AspectList;
 
 import java.util.concurrent.ConcurrentHashMap;
 
 @Mixin(ThaumcraftApi.class)
 public class ThaumcraftApiMixin {
+
+    @Inject(method = "registerObjectTag(Lnet/minecraft/item/ItemStack;Lthaumcraft/api/aspects/AspectList;)V", at = @At("HEAD"), remap = false, cancellable = true)
+    private static void disallowEmptyStacks(ItemStack item, AspectList aspects, CallbackInfo ci) {
+        if (item == null || item.isEmpty()) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "registerComplexObjectTag(Lnet/minecraft/item/ItemStack;Lthaumcraft/api/aspects/AspectList;)V", at = @At("HEAD"), remap = false, cancellable = true)
+    private static void disallowComplexEmptyStacks(ItemStack item, AspectList aspects, CallbackInfo ci) {
+        if (item == null || item.isEmpty()) {
+            ci.cancel();
+        }
+    }
 
     @Redirect(method = "exists", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ConcurrentHashMap;get(Ljava/lang/Object;)Ljava/lang/Object;", remap = false), remap = false)
     private static Object retrieve(ConcurrentHashMap concurrentHashMap, Object key, ItemStack stack) {
