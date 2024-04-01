38161a21e11e0339c95cd63efeff0196d3d39ce0
Wong, Dylan <dylanwong@nevada.unr.edu>
2024-04-01 00:03:05 -0700

    Update for modern mixins

--- src/main/java/zone/rong/thaumicspeedup/mixins/ThaumcraftApiMixin.java
+++ src/main/java/zone/rong/thaumicspeedup/mixins/ThaumcraftApiMixin.java
@@ -2,35 +2,54 @@ package zone.rong.thaumicspeedup.mixins;
 
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
 import thaumcraft.api.ThaumcraftApi;
-import thaumcraft.api.aspects.AspectEventProxy;
 import thaumcraft.api.aspects.AspectList;
 import zone.rong.thaumicspeedup.ThaumicSpeedup;
 
 @Mixin(value = ThaumcraftApi.class, remap = false)
 public class ThaumcraftApiMixin {
 
-    @Redirect(method = "*", at = @At(value = "NEW", target = "Lthaumcraft/api/aspects/AspectEventProxy;<init>()V"))
-    private static AspectEventProxy useProxyInstanceSingleton() {
-        return ThaumicSpeedup.PROXY_INSTANCE;
+    /**
+     * @author SumTingWong
+     * @reason Thaumcraft deprecated this method in version 6 with an event in AspectEventProxy to replace it
+     */
+    @Deprecated
+    @Overwrite
+    public static void registerObjectTag(ItemStack item, AspectList aspects) {
+        if (item == null || item.isEmpty()) return;
+        ThaumicSpeedup.PROXY_INSTANCE.registerObjectTag(item, aspects);
     }
 
-    @Inject(method = "registerObjectTag(Lnet/minecraft/item/ItemStack;Lthaumcraft/api/aspects/AspectList;)V", at = @At("HEAD"), cancellable = true)
-    private static void disallowEmptyStacks(ItemStack item, AspectList aspects, CallbackInfo ci) {
-        if (item == null || item.isEmpty()) {
-            ci.cancel();
-        }
+    /**
+     * @author SumTingWong
+     * @reason Thaumcraft deprecated this method in version 6 with an event in AspectEventProxy to replace it
+     */
+    @Deprecated
+    @Overwrite
+    public static void registerObjectTag(String oreDict, AspectList aspects) {
+        ThaumicSpeedup.PROXY_INSTANCE.registerObjectTag(oreDict, aspects);
     }
 
-    @Inject(method = "registerComplexObjectTag(Lnet/minecraft/item/ItemStack;Lthaumcraft/api/aspects/AspectList;)V", at = @At("HEAD"), cancellable = true)
-    private static void disallowComplexEmptyStacks(ItemStack item, AspectList aspects, CallbackInfo ci) {
-        if (item == null || item.isEmpty()) {
-            ci.cancel();
-        }
+    /**
+     * @author SumTingWong
+     * @reason Thaumcraft deprecated this method in version 6 with an event in AspectEventProxy to replace it
+     */
+    @Deprecated
+    @Overwrite
+    public static void registerComplexObjectTag(ItemStack item, AspectList aspects) {
+        if (item == null || item.isEmpty()) return;
+        ThaumicSpeedup.PROXY_INSTANCE.registerComplexObjectTag(item, aspects);
+    }
+
+    /**
+     * @author SumTingWong
+     * @reason Thaumcraft deprecated this method in version 6 with an event in AspectEventProxy to replace it
+     */
+    @Deprecated
+    @Overwrite
+    public static void registerComplexObjectTag(String oreDict, AspectList aspects) {
+        ThaumicSpeedup.PROXY_INSTANCE.registerComplexObjectTag(oreDict, aspects);
     }
 
 }
