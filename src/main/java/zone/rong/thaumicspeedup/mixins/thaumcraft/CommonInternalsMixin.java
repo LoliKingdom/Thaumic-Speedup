c6279262630199d163c8b5b82c463a9ec06c6066
Rongmario <rongmario@proton.me>
2026-06-15 09:56:39 +0100

    Removed negligible optimization & improved stack ID optimization

--- src/main/java/zone/rong/thaumicspeedup/mixins/thaumcraft/CommonInternalsMixin.java
+++ src/main/java/zone/rong/thaumicspeedup/mixins/thaumcraft/CommonInternalsMixin.java
@@ -1,35 +1,42 @@
 package zone.rong.thaumicspeedup.mixins.thaumcraft;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
 import thaumcraft.api.internal.CommonInternals;
 
 @Mixin(value = CommonInternals.class, remap = false)
 public class CommonInternalsMixin {
 
-    /**
-     * @author Rongmario
-     * @reason Eliminate ItemStack#copy + Use NBTTagCompound's native hashCode implementation
-     */
-    @Overwrite
-    public static int generateUniqueItemstackId(ItemStack stack) {
-        NBTTagCompound tag = stack.serializeNBT();
-        tag.removeTag("Count");
-        return tag.hashCode();
+    @Unique
+    private static int thaumicspeedup$id(ItemStack stack) {
+        return Item.getIdFromItem(stack.getItem()) * 92821 + stack.getItemDamage();
     }
 
     /**
      * @author Rongmario
-     * @reason Eliminate ItemStack#copy + Use NBTTagCompound's native hashCode implementation
+     * @reason Hash id, damage, tag directly, avoid copying ItemStack copy and creating NBT string per call
+     */
+    @Overwrite
+    public static int generateUniqueItemstackId(ItemStack stack) {
+        int hash = thaumicspeedup$id(stack);
+        NBTTagCompound tag = stack.getTagCompound();
+        if (tag != null) {
+            hash = hash * 92821 + tag.hashCode();
+        }
+        return hash;
+    }
+
+    /**
+     * @author Rongmario
+     * @reason Hash id, damage, tag directly, avoid copying ItemStack copy
      */
     @Overwrite
     public static int generateUniqueItemstackIdStripped(ItemStack stack) {
-        NBTTagCompound tag = stack.serializeNBT();
-        tag.removeTag("Count");
-        tag.removeTag("tag");
-        return tag.hashCode();
+        return thaumicspeedup$id(stack);
     }
 
 }
